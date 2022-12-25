79a858914c5ba4c052079cba6bf800a23fd31bc6
Nadezhda <402630@bk.ru>
2022-12-25 18:10:36 +0700

    Изменен номер задачи с 02 на 03

---
+++ Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая будет выдавать
+название дня недели по заданному номеру.
+3 -> Среда
+5 -> Пятница*/
+
+Console.WriteLine("Введите число в диапазоне от 1 до 7:");
+string number = (Console.ReadLine());
+if (number == "1") Console.WriteLine("понедельник");
+else if (number == "2") Console.WriteLine("вторник");
+else if (number == "3") Console.WriteLine("среда");
+else if (number == "4") Console.WriteLine("четверг");
+else if (number == "5") Console.WriteLine("пятница");
+else if (number == "6") Console.WriteLine("суббота");
+else if (number == "7") Console.WriteLine("воскресенье");
+else Console.WriteLine("Не бывает такого дня недели");
