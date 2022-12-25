0c3472209d1f2940c2cc7d23b40345d7e91bfb2c
Nadezhda <402630@bk.ru>
2022-12-25 20:46:09 +0700

    Задача 06 решена

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -5,4 +5,8 @@
  7 -> нет*/
 
 
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
 
+if (number % 2 == 0) Console.WriteLine($"Число {number} является чётным");
+else Console.WriteLine($"Число {number} является нечётным");
