fc8b7647d0e8eedc470cb5865b241a98270853c7
Nadezhda <402630@bk.ru>
2022-12-25 20:37:48 +0700

    Задача 04 решена

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -4,3 +4,16 @@
 44 5 78 -> 78
 22 3 9 -> 22*/
 
+Console.WriteLine("Введите первое число");
+int first_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int second_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int third_number = Convert.ToInt32(Console.ReadLine());
+
+int max = first_number;
+
+if (second_number > max) max = second_number;
+if (third_number > max) max = third_number;
+
+Console.WriteLine($"Максимальное из этих чисел {max}");
