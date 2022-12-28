c8f4a834e741e80fb5c33ae1842f0080c72b8477
Nadezhda <402630@bk.ru>
2022-12-28 18:58:51 +0700

    Задача 11 решена

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая 
+1. выводит случайное трёхзначное число и 
+2. удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98*/
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трёхзначное число -> {number}");
+
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+
+int result = firstDigit * 10 + thirdDigit;
+
+Console.WriteLine($"Результат -> {result}");
+
