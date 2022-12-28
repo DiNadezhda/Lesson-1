51aaeff52a43ba9fcc2a51ee4ffef059afb4f0c9
Nadezhda <402630@bk.ru>
2022-12-28 18:03:07 +0700

    Задача 09 решена при помощи метода

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -9,8 +9,24 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
 
-if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа = {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа => {secondDigit}");
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа = {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа => {secondDigit}");
+
+// Console.Write($"Наибольшая цифра числа => ");
+// Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
+
+int maxDigit = MaxDigit(number);
+Console.Write($"Наибольшая цифра числа => {maxDigit}");
+
+
+int MaxDigit(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return result;
+    ;
+}
