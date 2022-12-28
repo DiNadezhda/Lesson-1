e32a09ef7b51cf39582229a2e4c7e0342fc8628a
Nadezhda <402630@bk.ru>
2022-12-28 19:57:12 +0700

    Задача 16 решена при помощи метода

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -6,3 +6,15 @@
 -4, 16 -> да
 25, 5 -> да
 8,9 -> нет*/
+
+Console.WriteLine("Введите два числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SquareNumber(number1, number2) ? "Да" : "Нет");
+
+
+bool SquareNumber(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
