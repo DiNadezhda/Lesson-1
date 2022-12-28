8a8fd54fbf2131f5961681458f0a10ca5cfe455b
Nadezhda <402630@bk.ru>
2022-12-28 19:28:33 +0700

    Задача 12 решена с применением метода

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -9,6 +9,13 @@
 Console.WriteLine("Введите два числа: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
+int res = Multiplicity(number1, number2);
+Console.WriteLine(res == 0 ? "Кратно" : $"Не кратно, остаток от деления {res}");
 
-if (number1 % number2 == 0) Console.WriteLine("кратно");
-else Console.WriteLine($"не кратно, остаток {number1 % number2}");
+// if (number1 % number2 == 0) Console.WriteLine("кратно");
+// else Console.WriteLine($"не кратно, остаток {number1 % number2}");
+
+int Multiplicity(int num1, int num2)
+{
+    return num1 % num2;
+}
