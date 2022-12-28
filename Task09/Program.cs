52183d82db0be0c9d795dd0df6f2e8560669d0d4
Nadezhda <402630@bk.ru>
2022-12-28 17:06:02 +0700

    Решена задача 09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -7,3 +7,10 @@
 85 -> 8*/
 
 int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа = {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа => {secondDigit}");
