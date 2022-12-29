1407100ecb553a729a7a0329675884bc09ea5423
Nadezhda <402630@bk.ru>
2022-12-29 18:23:43 +0700

    Задача 22 решена с использованием метода

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая
+1. принимает на вход число (N) и 
+2. выдаёт таблицу квадратов чисел от 1 до N.*/
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0) TableSquare(number);
+else Console.WriteLine("Некорректные данные");
+
+
+void TableSquare(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count, 4} {Math.Pow(count, 2), 6}");
+        count++;
+    }
+}
