08a0b39015fc3daee328dea81a4e0dbb50d29d47
Nadezhda <402630@bk.ru>
2022-12-29 12:29:30 +0700

    Задача 15 решена

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -3,3 +3,10 @@
 6 -> да
 7 -> да
 1 -> нет*/
+
+Console.WriteLine("Введите цифру: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 6 || number == 7) Console.WriteLine("да");
+else if (number > 0 && number < 6) Console.WriteLine("нет");
+else if (number < 0 || number > 7) Console.WriteLine("не существует такого дня недели");
