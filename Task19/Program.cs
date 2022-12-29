1be4316d4b0ef4f07fa622d83ee6cdf876d04ccf
Nadezhda <402630@bk.ru>
2022-12-29 21:37:47 +0700

    Задача 19 решена с использованием метода

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,3 +6,26 @@
 12821 -> да
 
 23432 -> да*/
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+if (number >= 10000 && number < 100000) // проверка на 5-значность
+{
+    Console.Write($"{number} -> ");
+    Console.Write(Palindrome(number) ? "да" : "нет");
+}
+else Console.WriteLine("Введены некорректные данные");
+
+
+// метод, сравнивающий симметричные цифры числа
+bool Palindrome(int num)
+{
+    if (num / 10000 == num % 10
+        && num / 1000 % 10 == num / 10 % 10) return true;
+    else return false;
+}
+
