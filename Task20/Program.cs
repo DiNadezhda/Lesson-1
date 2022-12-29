82563fb5ebff6fe3664165c8d250c1910dd21598
Nadezhda <402630@bk.ru>
2022-12-29 17:47:22 +0700

    Задача 20 решена с использованием метода

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая
+1. принимает на вход координаты двух точек и
+2. находит расстояние между ними в 2D
+пространстве.
+
+A (3,6); B (2,1) -> 5,09
+A (7,-5); B (1,-1) -> 7,21*/
+
+//double d = Math.Sqrt(num) // корень 
+
+// Math.Pow(2, 2);
+// double d = Math.Sqrt(355); // квадрат
+
+// double num = 5.099873456;
+// double numRound = Math.Round(num, 2, MidpointRounding.ToZero);
+// Console.WriteLine(numRound); // округление
+
+Console.WriteLine("Введите координаты двух точек: ");
+Console.WriteLine("Ax: ");
+int ax = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ay: ");
+int ay = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Bx: ");
+int bx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("By: ");
+int by = Convert.ToInt32(Console.ReadLine());
+
+double ourDistance = Math.Round(Distance(ax, ay, bx, by), 2, MidpointRounding.ToZero);
+Console.WriteLine($"Расстояние между точками A и B равно -> {ourDistance}");
+
+
+
+double Distance (int x1, int y1, int x2, int y2)
+{
+    double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+    return distance;
+}
