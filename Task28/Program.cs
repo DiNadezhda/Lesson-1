849c4faf1d887ad86794301077e561092c73c650
Nadezhda <402630@bk.ru>
2023-01-15 14:31:58 +0700

    Задача 28 решена

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -3,3 +3,21 @@
 произведение чисел от 1 до N.
 4 -> 24
 5 -> 120*/
+
+Console.Write ("Введите целое число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+// int number1 = int.Parse(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {sumNumbers}");
+
+int SumNumbers(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        //sum = sum + i;
+        sum *= i;
+    }
+    return sum;
+} 
