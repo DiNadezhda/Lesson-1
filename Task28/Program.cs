4d23db3670964269fc298ec1d87f3310fc16c04d
Nadezhda <402630@bk.ru>
2023-01-15 15:03:38 +0700

    В решение задачи 28 добавлена проверка на переполнение checked

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -8,16 +8,18 @@ Console.Write ("Введите целое число: ");
 int number = Convert.ToInt32 (Console.ReadLine());
 // int number1 = int.Parse(Console.ReadLine());
 
-int sumNumbers = SumNumbers(number);
-Console.WriteLine($"Произведение чисел от 1 до {number} = {sumNumbers}");
+int factorialNumbers = FactorialNumbers(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {factorialNumbers}");
 
-int SumNumbers(int num)
+int FactorialNumbers(int num)
 {
-    int sum = 1;
+    int factorial = 1;
     for (int i = 1; i <= num; i++)
-    {
-        //sum = sum + i;
-        sum *= i;
+    { 
+        checked // проверка на переполнение
+        {
+            factorial *= i; // factorial = factorial * i;
+        }
     }
-    return sum;
+    return factorial;
 } 
