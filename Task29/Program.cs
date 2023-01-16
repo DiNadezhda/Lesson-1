4d05c1e5cc83842a60d7f9a710335f69bf89627d
Nadezhda <402630@bk.ru>
2023-01-16 16:26:38 +0700

    Задача 29 решена

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -5,3 +5,43 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]*/
 
+Console.WriteLine("Введите минимальное число массива");
+int minNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное число массива");
+int maxNumber = Convert.ToInt32(Console.ReadLine());
+
+//int[] array = new int[8];
+
+int[] array = CreateArray(8, minNumber, maxNumber);
+PrintArray(array, 8);
+
+//Метод создания рандомного массива
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+
+//Метод вывода на печать массива в квадратных скобках
+void PrintArray(int[] arr, int size)
+{
+    //Вывод массива через запятую (левая часть задания)
+    for (int i = 0; i < size-1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[size - 1] + " -> ");
+
+    //Вывод массива в квадратных скобках, через запятую (правая часть задания)
+    Console.Write("[");
+    for (int i = 0; i < size - 1; i++)
+    {
+        Console.Write(arr[i] + ", "); 
+    }
+    Console.Write(arr[size - 1] + "]");
+}
