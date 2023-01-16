591f92b5235bd15ad47e4d6fbc6044aca1bec87d
Nadezhda <402630@bk.ru>
2023-01-16 15:08:05 +0700

    Задача 27 решена

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -5,3 +5,20 @@
 82 -> 10
 9012 -> 12*/
 
+Console.Write ("Введите целое число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+int sumDigit = SumDigit(number);
+Console.WriteLine($"Сумма цифр в числе {number} -> {sumDigit}");
+
+//Метод, выдающий сумму цифр в числе
+int SumDigit(int num)
+{
+    int sum = 0;
+    for (int i = 0; i <= 10; i++)
+    {
+        sum = sum + ( num % 10 );
+        num = num / 10;
+    }
+    return sum;
+} 
