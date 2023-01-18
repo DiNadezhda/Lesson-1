eb5c0fa00de321b65cff7f9366da860ed7b7b01f
Nadezhda <402630@bk.ru>
2023-01-18 21:02:06 +0700

    Задача 37 решена

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 35: 
+Задайте одномерный массив из 123 случайных чисел.
+Найдите количество элементов массива, значения которых лежат в
+отрезке [10,99].
+
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5*/
+
+
+int[] array = CreateArrayRndInt(123, -100, 100);
+PrintArray(array);
+int countElements = CountElements(array);
+Console.WriteLine(countElements);
+
+
+//Метод, сздающий массив
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    var rnd = new Random(); // var когда в правой части ясно какой тип данных мы используем
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+//Вывод массива в терминал
+void PrintArray(int[] arr)
+{
+    Console.Write('[');
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+//Метод подсчёта двузначных элементов в заданном массиве
+int[] CountElements(int[] arr, int min = 10, int max = 99)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= min && arr[i] <= max) count++;
+    }
+    return count;
+}
+
--- Task37/Program.cs
+++ Task37/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 37: 
+Найдите произведение пар чисел в одномерном массиве.
+Парой считаем первый и последний элемент, второй и предпоследний
+и т.д. Результат запишите в новом массиве.
+
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21*/
+
+int[] array = CreateArrayRndInt(5, 1, 8);
+PrintArray(array);
+int[] newArray = MultiplicationOfNumbersArray(array);
+PrintArray(newArray);
+
+
+//Метод, сздающий массив
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    var rnd = new Random(); // var когда в правой части ясно какой тип данных мы используем
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+//Вывод массива в терминал
+void PrintArray(int[] arr)
+{
+    Console.Write('[');
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+//Метод, возвращающий новый массив, перемножающий пары элементов в исходном массиве
+int[] MultiplicationOfNumbersArray(int[] arr)
+{
+    int size = arr.Length / 2;
+    if (arr.Length % 2 == 1) size += 1;
+
+    int[] resArr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        if (i != arr.Length - 1 - i)
+            resArr[i] = arr[i] * arr[arr.Length - 1 - i];
+        else resArr[i] = arr[i];
+    }
+    return resArr;
+}
