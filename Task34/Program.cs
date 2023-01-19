c632df7c55e42abc6aa410c352aed052bbb12749
Nadezhda <402630@bk.ru>
2023-01-19 12:08:56 +0700

    Задача 34 решена

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -4,3 +4,44 @@
 
 [345, 897, 568, 234] -> 2*/
 
+
+int[] array = CreateArrayRndInt(4, 100, 999);
+PrintArray(array);
+int countEvenNumber = CountEvenNumber(array);
+Console.WriteLine($" -> {countEvenNumber}");
+
+
+//Метод, создающий массив
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    var rnd = new Random(); // var когда в правой части ясно какой тип данных мы используем
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+//Вывод массива в терминал
+void PrintArray(int[] arr)
+{
+    Console.Write('[');
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+//Метод подсчёта чётных чисел в  массиве
+int CountEvenNumber(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count++;
+    }
+    return count;
+}
