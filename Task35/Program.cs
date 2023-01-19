5b2689dd67a1ba0e204e7b74323dea00c2735777
Nadezhda <402630@bk.ru>
2023-01-19 11:51:56 +0700

    Создана задача 34

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -16,7 +16,7 @@ int countElements = CountElements(array);
 Console.WriteLine($" -> {countElements}");
 
 
-//Метод, сздающий массив
+//Метод, создающий массив
 int[] CreateArrayRndInt(int size, int min, int max)
 {
     int[] arr = new int[size];
@@ -51,3 +51,30 @@ int CountElements(int[] arr, int min = 10, int max = 99)
     return count;
 }
 
+
+
+
+//Метод, создающий массив из вещественных чисел
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    var rnd = new Random(); // var когда в правой части ясно какой тип данных мы используем
+    for (int i = 0; i < arr.Length; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min;
+        arr[i] = Math.Round(num, 1);
+    }
+    return arr;
+}
+
+//Вывод массива в терминал
+void PrintArrayDouble(double[] arr)
+{
+    Console.Write('[');
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
