2639f15875cb1d4d0da7a674a205228e10aeb3d1
Nadezhda <402630@bk.ru>
2023-01-19 12:19:28 +0700

    Задача 36 решена

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -6,3 +6,44 @@
 
 [-4, -6, 89, 6] -> 0*/
 
+int[] array = CreateArrayRndInt(4, -10, 10);
+PrintArray(array);
+
+int sumOfOddElements = SumOfOddElements(array);
+Console.WriteLine($" -> {sumOfOddElements}");
+
+
+//Метод, создающий массив
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    var rnd = new Random(); // var когда в правой части ясно какой тип данных мы используем
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+//Вывод массива в терминал
+void PrintArray(int[] arr)
+{
+    Console.Write('[');
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+//Сумма элементов, стоящих на нечётных позициях
+int SumOfOddElements(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) sum += arr[i];
+    }
+    return sum;
+}
