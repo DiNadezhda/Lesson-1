18cdbe6aad00bf69f99dde27c6dd125b5c716ce7
Nadezhda <402630@bk.ru>
2023-01-19 11:56:02 +0700

    Создана задача 38

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -54,27 +54,3 @@ int CountElements(int[] arr, int min = 10, int max = 99)
 
 
 
-//Метод, создающий массив из вещественных чисел
-double[] CreateArrayRndDouble(int size, int min, int max)
-{
-    double[] arr = new double[size];
-    var rnd = new Random(); // var когда в правой части ясно какой тип данных мы используем
-    for (int i = 0; i < arr.Length; i++)
-    {
-        double num = rnd.NextDouble() * (max - min) + min;
-        arr[i] = Math.Round(num, 1);
-    }
-    return arr;
-}
-
-//Вывод массива в терминал
-void PrintArrayDouble(double[] arr)
-{
-    Console.Write('[');
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
-        else Console.Write(arr[i]);
-    }
-    Console.Write("]");
-}
---
+++ Task38/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 38: 
+1. Задайте массив вещественных чисел. 
+2. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2*/
+
+//Метод, создающий массив из вещественных чисел
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    var rnd = new Random(); // var когда в правой части ясно какой тип данных мы используем
+    for (int i = 0; i < arr.Length; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min;
+        arr[i] = Math.Round(num, 1);
+    }
+    return arr;
+}
+
+//Вывод массива в терминал
+void PrintArrayDouble(double[] arr)
+{
+    Console.Write('[');
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
