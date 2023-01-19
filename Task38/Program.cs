3c6a63f4b75b52e9389e3e78ae37f3a9e13283d7
Nadezhda <402630@bk.ru>
2023-01-19 12:51:25 +0700

    Задача 38 решена

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,8 +1,16 @@
 ﻿/*Задача 38: 
 1. Задайте массив вещественных чисел. 
-2. Найдите разницу между максимальным и минимальным элементов массива.
+2. Найдите разницу между максимальным и минимальным элементами массива.
 
 [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2*/
+
+
+double[] array = CreateArrayRndDouble(5, 1, 99);
+PrintArrayDouble(array);
+
+double maxElemArray = MaxElemArray(array);
+double minElemArray = MinElemArray(array);
+Console.WriteLine($" -> {maxElemArray - minElemArray}");
 
 //Метод, создающий массив из вещественных чисел
 double[] CreateArrayRndDouble(int size, int min, int max)
@@ -23,8 +31,30 @@ void PrintArrayDouble(double[] arr)
     Console.Write('[');
     for (int i = 0; i < arr.Length; i++)
     {
-        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        if (i < arr.Length - 1) Console.Write(arr[i] + "; ");
         else Console.Write(arr[i]);
     }
     Console.Write("]");
+}
+
+//Поиск максимального элемента массива
+double MaxElemArray(double[] arr)
+{
+    double max = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+//Поиск минимального элемента массива
+double MinElemArray(double[] arr)
+{
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+    }
+    return min;
 }
