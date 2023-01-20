716e35310c9a01b3dc96bac143960b76a6212b7d
Nadezhda <402630@bk.ru>
2023-01-20 18:53:54 +0700

    Задача 42 решена

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -6,3 +6,20 @@
 3 -> 11
 2 -> 10*/
 
+Console.WriteLine("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string result = ConvertDecToBin(number);
+Console.WriteLine($"{number} -> {result}");
+
+//Метод, преобразующий десятичное число в двоичное
+string ConvertDecToBin(int num)
+{
+    string res = string.Empty; //задали пустую строку
+    while (num > 0)
+    {
+        res = num % 2 + res;
+        num = num / 2;
+    }
+    return res;
+}
