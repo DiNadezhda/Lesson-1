b643be1ec521c1466f91361b9734e1f7070214eb
Nadezhda <402630@bk.ru>
2023-01-21 11:24:55 +0700

    Задача 45 решена

--- Task45/Program.cs
+++ Task45/Program.cs
@@ -3,3 +3,44 @@
 копию заданного массива с помощью поэлементного
 копирования.*/
 
+
+int[] arrayOrigin = CreateArrayRndInt(10, -10, 10);
+PrintArray(arrayOrigin);
+int[] arrayCopy = CopyArray(arrayOrigin);
+PrintArray(arrayCopy);
+
+
+//Метод, создающий массив
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    var rnd = new Random(); // var когда в правой части ясно какой тип данных мы используем
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+//Метод, создающий копию массива
+int[] CopyArray(int[] arr)
+{
+    int[] result = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result[i] = arr[i];
+    }
+    return result;
+}
+
+//Вывод массива в терминал
+void PrintArray(int[] arr)
+{
+    Console.Write('[');
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
