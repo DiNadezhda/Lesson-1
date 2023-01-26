28ffda38e6eae41b6f730974665b2b24f8ca7329
Nadezhda <402630@bk.ru>
2023-01-26 14:59:59 +0700

    Задача 49 решена

--- Task49/Program.cs
+++ Task49/Program.cs
@@ -12,3 +12,53 @@
  1 4 49 2
  5 9 2 3
 64 4 4 4*/
+
+//Метод, создающий двуменый массив
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+//Метод, возвращающий двумерный массив, в котором
+//оба индекса чётные, и заменены эти элементы на их квадраты
+void FindEvenIndex(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+//Вывод двумерного массива в терминал
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3},");
+            else Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] matrix1 = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(matrix1);
+Console.WriteLine();
+FindEvenIndex(matrix1);
+PrintMatrix(matrix1);
