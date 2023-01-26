69739ae5952ceb728c64f2af689d7c788ec403d0
Nadezhda <402630@bk.ru>
2023-01-26 13:27:08 +0700

    Задача 48 решена

--- Task48/Program.cs
+++ Task48/Program.cs
@@ -9,3 +9,37 @@ m = 3, n = 4.
 1 2 3 4
 2 3 4 5*/
 
+
+int[,] matrixSumInd = CreateMatrixSumIndex(3, 4);
+PrintMatrix(matrixSumInd);
+
+//Метод, создающий двумерный массив
+int[,] CreateMatrixSumIndex(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+//Вывод двумерного массива в терминал
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
