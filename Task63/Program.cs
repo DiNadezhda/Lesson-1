963aa638d89f6ab7ce6a4ee1128ed90d35f2a767
Nadezhda <402630@bk.ru>
2023-02-06 14:55:44 +0700

    Решена задача 63

--- Task63/Program.cs
+++ Task63/Program.cs
@@ -6,3 +6,14 @@
 N = 5 -> "1, 2, 3, 4, 5"
 N = 6 -> "1, 2, 3, 4, 5, 6" */
 
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number);
+
+void NaturalNumbers (int num)
+{
+    if(num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
