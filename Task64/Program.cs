5528ccb86b3c6d07b78967ba760dd1edee030e76
Nadezhda <402630@bk.ru>
2023-02-06 19:16:08 +0700

    Решена задача 64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -6,3 +6,19 @@
 
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"N = {number} -> « ");
+if (number > 0) NaturalNumbersRevers(number);
+else Console.WriteLine("Ошибка ввода числа");
+Console.WriteLine("»");
+
+
+void NaturalNumbersRevers (int num)
+{
+    if(num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbersRevers(num - 1);
+}
