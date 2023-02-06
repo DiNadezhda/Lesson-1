be1876fd1d765722c475d92ba1d7d87dfb22c06d
Nadezhda <402630@bk.ru>
2023-02-06 19:29:10 +0700

    Решена задача 66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -8,3 +8,19 @@ M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
 
+Console.Write("Введите первое число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(" -> ");
+if (numberM < numberN) Console.WriteLine(SumNaturalElem (numberM, numberN));
+else Console.WriteLine(SumNaturalElem (numberN, numberM));
+
+// Сумма натуральных элементов в промежутке от M до N. Рекурсия
+int SumNaturalElem(int numM, int numN)
+{
+    if (numM == numN) return numM;
+    else return numN + SumNaturalElem (numM, numN - 1);
+}
