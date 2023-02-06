fb1516aeea53c26cc8c40dc4ee9125285a4c62b6
Nadezhda <402630@bk.ru>
2023-02-06 18:10:41 +0700

    Решена задача 69

--- Task69/Program.cs
+++ Task69/Program.cs
@@ -5,3 +5,21 @@
 A = 3; B = 5 -> 243 (3⁵)
 A = 2; B = 3 -> 8 */
 
+Console.Write("Введите первое число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число В: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB > 0)
+{
+    Console.WriteLine(MathPow(numberA, numberB));
+}
+else Console.WriteLine("Ошибка ввода степени");
+
+// Возведение в степень. Рекурсия
+int MathPow(int num, int degree)
+{
+    if (degree == 0) return 1;
+    else return num * MathPow(num, degree - 1);
+}
