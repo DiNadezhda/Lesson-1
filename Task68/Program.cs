a33ac03b0f87349e7020deabe717c4730efceddc
Nadezhda <402630@bk.ru>
2023-02-06 19:51:42 +0700

    Решена задача 68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -3,3 +3,19 @@
 рекурсии. Даны два неотрицательных числа m и n.
 
 m = 3, n = 2 -> A(m,n) = 29 */
+
+Console.Write("Введите первое число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(String.Empty);
+Console.WriteLine($" -> A({numberM},{numberN}) = {Ackerman(numberM, numberN)}");
+
+int Ackerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if ((m != 0) && (n == 0)) return Ackerman(m - 1, 1);
+    else return Ackerman(m - 1, Ackerman(m, n - 1));
+}
